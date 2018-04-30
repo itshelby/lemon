1fb43e34339d8a85d9f3b613f242b8397b837abc
itshelby <neko.klaus.coder@gmail.com>
2018-04-30 22:31:00 +0300

    :heavy_plus_sign: add pug and browser-sync

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,7 +1,42 @@
-import gulp from 'gulp'
+import {src, dest, watch, parallel, series, task} from 'gulp'
+import browserSync from 'browser-sync'
+import pug from 'gulp-pug'
 
-export function hello() {
-  console.log('Hello!')
+// Build Directories
+// ----
+const dirs = {
+  src: 'src',
+  dest: 'dist'
 }
 
-export default hello()
+// File Sources
+// ---- 
+const sources = {
+  views: `${dirs.src}/**/*.pug`
+}
+
+// Main Tasks
+// ----
+
+// Run server
+export const server = () => {
+  browserSync({
+    server: {
+      baseDir: 'dist'
+    },
+    notify: false
+  })
+  watch(sources.views, buildViews)
+}
+
+// Views
+export const buildViews = () => src(sources.views)
+  .pipe(pug())
+  .pipe(dest(dirs.dest))
+  .pipe(browserSync.stream())
+
+// Development Task
+export const dev = series(server)
+
+// Default task
+export default dev
